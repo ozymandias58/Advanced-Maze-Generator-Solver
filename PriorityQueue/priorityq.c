5934273480d2f1606b43429ca5b48ed4d89b5a68
Abdullah Yasin Yılmaz <abdullahysnylmz684@gmail.com>
2025-12-22 12:50:20 +0300

    Dijkstranin algoritmasi icin gerekli priority queue (heap) yapisini olusturdum

---
+++ PriorityQueue/priorityq.c
@@ -0,0 +1,98 @@
+#include "priorityq.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//initHeap() first
+//insert(weight,vertex) and extract() as much as you want
+//freeHeap() after you are done
+
+MinHeap h;
+
+void initHeap(){
+    h.capacity = HEAPINITIALCAP;
+    h.size = 0;
+    h.arr = (PQNode*)malloc(sizeof(PQNode)*h.capacity);
+}
+
+void freeHeap(){
+    free(h.arr);
+}
+
+int getLeftIndex(int index){
+    return (index*2)+1;}
+int getRightIndex(int index){
+    return (index*2)+2;}
+int getParentIndex(int index){
+    return (index-1)/2;}
+int hasLeft(int index){
+    return getLeftIndex(index) < h.size;}
+int hasRight(int index){
+    return getRightIndex(index) < h.size;}
+int hasParent(int index){
+    return getParentIndex(index) < h.size;}
+PQNode getLeft(int index){
+    return h.arr[getLeftIndex(index)];}
+PQNode getRight(int index){
+    return h.arr[getRightIndex(index)];}
+PQNode getParent(int index){
+    return h.arr[getParentIndex(index)];}
+
+void swapPQ(PQNode *a, PQNode *b){
+    PQNode t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void heapifyDown(){
+    int index = 0;
+    while(hasLeft(index)){
+        int smallerChildIndex = getLeftIndex(index);
+        if(hasRight(index) && getRight(index).distance < h.arr[smallerChildIndex].distance)
+            smallerChildIndex = getRightIndex(index);
+        if(h.arr[smallerChildIndex].distance > h.arr[index].distance)
+            break;
+        swapPQ(&(h.arr[index]),&(h.arr[smallerChildIndex]));
+        index = smallerChildIndex;
+    }
+}
+
+void heapifyUp(){
+    int index = h.size-1;
+    while(hasParent(index) && getParent(index).distance > h.arr[index].distance){
+        swapPQ(&(h.arr[index]),&(h.arr[getParentIndex(index)]));
+        index = getParentIndex(index);
+    }
+}
+
+PQNode extract(){
+    if(h.size == 0){
+        printf("\n priority queue is empty");
+        PQNode emptyNode = {-1,-1};
+        return emptyNode ;}
+    PQNode item = h.arr[0];
+    h.arr[0] = h.arr[h.size-1];
+    h.size--;
+    heapifyDown();
+    return item;
+}
+
+void insert(int weight, int vertex){
+    if(h.size == h.capacity){
+        h.capacity *= 2;
+        h.arr = realloc(h.arr,sizeof(PQNode)*h.capacity);
+    }
+    PQNode node = {weight,vertex};
+    h.arr[h.size++] = node;
+    heapifyUp();
+}
+
+void printHeap(){
+    if(h.size == 0)
+        return;
+        printf("\n Priority Queue: ");
+    for(int i=0; i<h.size;i++){
+        printf("%d, ", h.arr[i].distance);
+    }
+    printf("\n");
+}
---
+++ PriorityQueue/priorityq.h
@@ -0,0 +1,35 @@
+#ifndef PRIORITYQ_H
+#define PRIORITYQ_H
+
+#define HEAPINITIALCAP 100
+
+typedef struct PQNode{
+    int distance;
+    int vertex;
+}PQNode;
+
+typedef struct MinHeap{
+    int capacity;
+    int size;
+    PQNode* arr;
+}MinHeap;
+
+void initHeap();
+void freeHeap();
+int getLeftIndex(int index);
+int getRightIndex(int index);
+int getParentIndex(int index);
+int hasLeft(int index);
+int hasRight(int index);
+int hasParent(int index);
+PQNode getLeft(int index);
+PQNode getRight(int index);
+PQNode getParent(int index);
+void swapPQ(PQNode *a, PQNode *b);
+void heapifyDown();
+void heapifyUp();
+PQNode extract();
+void insert(int weight, int vertex);
+void printHeap();
+
+#endif
